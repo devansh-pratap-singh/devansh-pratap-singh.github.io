b2df413914d591ff0570ac12e6ae22d4062fd036
devansh-pratap-singh <devansh.pratap@gmail.com>
2026-07-13 12:13:21 -0500

    In Progress

--- src/components/shared/ExternalLink.jsx
+++
@@ -1,9 +0,0 @@
-export function ExternalLink({ href, className, children }) {
-  const props = {
-    href,
-    className,
-    target: `_blank`,
-    rel: `noopener noreferrer`,
-  };
-  return <a {...props}>{children}</a>;
-}
--- src/components/shared/SkillPill.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './SkillPill.module.css';
-
-export function SkillPill({ name, primary = false }) {
-  return (
-    <span className={`${styles.pill} ${primary ? styles.primary : styles.secondary}`}>
-      {name}
-    </span>
-  );
-}
